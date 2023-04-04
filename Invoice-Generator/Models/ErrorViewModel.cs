bb3a348b237060ec83db8bd81478bdc30b1a9b1a
Adam Kazmierczyk 106214 <73425590+adamus9050@users.noreply.github.com>
2023-04-04 19:38:40 +0200

    Delete Invoice-Generator directory

--- Invoice-Generator/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Invoice_Generator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
